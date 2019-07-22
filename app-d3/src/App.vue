99ac31600c075e06113d2c0b54769d2bacba4871
KarlaStefany <kalvaradosalazar@gmail.com>
2019-07-21 23:40:37 -0500

    :heart: first app with vue and D3

---
+++ app-d3/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+  <Char />
+  </div>
+</template>
+
+<script>
+import Char from './components/Char.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Char
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
